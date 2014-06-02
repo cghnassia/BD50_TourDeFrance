23cb333b0a4bf046ff1df6a690c947b612062dd2
t-simon <1115994@gmail.com>
2014-06-02 23:38:03 +0200

    MAJ trigger+ajout procédure insertion&fonction

--- Conception/Scripts/250_create_trigger.sql
+++ Conception/Scripts/250_create_trigger.sql
@@ -1,7 +1,9 @@
 -- -----------------------------------------------------------------------------
---       TRIGGER INSERTION AVEC SEQUENCE
+--       GESTION DES INSCRIPTIONS
 -- -----------------------------------------------------------------------------
 
+
+-- CYCLISTE --
 create or replace trigger TI_CYCLISTE
 before insert on CYCLISTE 
 for each row
@@ -12,6 +14,7 @@ begin
 end;
 /
 
+-- PAYS --
 create or replace trigger TI_PAYS
 before insert on PAYS 
 for each row
@@ -22,6 +25,7 @@ begin
 end;
 /
 
+-- DIRECTEUR_SPORTIF --
 create or replace trigger TI_DIRS
 before insert on DIRECTEUR_SPORTIF
 for each row
@@ -32,6 +36,65 @@ begin
 end;
 /
 
+-- EQUIPE --
+create or replace trigger TI_EQUIPE
+before insert on EQUIPE
+for each row
+declare
+nequipe number(2);
+nbequipe number(2);
+begin
+	if inserting then
+		select count(equ.equipe_num) into nbequipe from equipe equ where equ.tour_annee = :new.tour_annee;
+			if(nbequipe=0) then
+				nequipe:=0;
+			else
+				select max(equ.equipe_num) into nequipe from equipe equ where equ.tour_annee = :new.tour_annee;
+			end if;
+		:new.equipe_num:=nequipe+1;
+		:new.equipe_pays := returnPays(:new.pays_num);
+	end if; 
+end;
+/
+
+-- PARTICIPANT --
+create or replace trigger TI_PARTICIPANT
+before insert on PARTICIPANT
+for each row
+declare
+npart number(2);
+nbpart number(2);
+npays number(3);
+begin
+	if inserting then
+		select count(par.part_num) into nbpart from participant par where par.tour_annee = :new.tour_annee AND par.equipe_num = :new.equipe_num;
+		
+			if(nbpart=0) then
+				npart:=(:new.equipe_num-1)*10;
+			else
+				select max(par.part_num) into npart from participant par where par.tour_annee = :new.tour_annee AND par.equipe_num = :new.equipe_num;
+			end if;
+		:new.part_num:=npart+1;
+		
+		:new.cycliste_nom:=returnNomCycliste(:new.cycliste_num);
+		:new.cycliste_prenom:=returnPrenomCycliste(:new.cycliste_num);
+		:new.cycliste_daten:=returnDateCycliste(:new.cycliste_num);
+	
+		select cyc.pays_num into npays from cycliste cyc where cyc.cycliste_num = :new.cycliste_num;
+		:new.cycliste_pays:=returnPays(npays);
+		
+		:new.equipe_nom:=returnNomEquipe(:new.equipe_num);
+		
+	end if; 
+end;
+/
+
+-- -----------------------------------------------------------------------------
+--       GESTION DE LA COURSE
+-- -----------------------------------------------------------------------------
+
+
+-- VILLE --
 create or replace trigger TI_VILLE
 before insert on VILLE 
 for each row
@@ -42,85 +105,7 @@ begin
 end;
 /
 
-create or replace trigger TI_PAYS
-before insert on PAYS 
-for each row
-begin
-	if inserting then
-	select seq_pays.nextval into :new.pays_num from dual;
-	end if; 
-end;
-/
-
-create or replace trigger TI_CATEGORIE
-before insert on CATEGORIE 
-for each row
-begin
-	if inserting then
-	select seq_categorie.nextval into :new.cat_num from dual;
-	end if; 
-end;
-/
-
-create or replace trigger TI_UTILISATEUR
-before insert on UTILISATEUR
-for each row
-begin
-	if inserting then
-	select seq_utilisateur.nextval into :new.util_num from dual;
-	end if; 
-end;
-/
-
-create or replace trigger TI_CONTROLE
-before insert on CONTROLE
-for each row
-begin
-	if inserting then
-	select seq_controle.nextval into :new.contr_num from dual;
-	end if; 
-end;
-/
-
-create or replace trigger TI_SPECIALISTE
-before insert on SPECIALISTE 
-for each row
-begin
-	if inserting then
-	select seq_specialiste.nextval into :new.spe_num from dual;
-	end if; 
-end;
-/
-
--- -----------------------------------------------------------------------------
---       TRIGGER INSERTION AVEC DONNEES CALCULEES ET REDONDANTES
--- -----------------------------------------------------------------------------
-
-create or replace trigger TI_POINT_PASSAGE
-before insert on POINT_PASSAGE
-for each row
-declare
-distance number(3);
-km_arr number(3);
-npp number(3);
-nbpp number(3);
-begin
-	if inserting then
-		select count(pp.pt_pass_num) into nbpp from point_passage pp where pp.tour_annee = :new.tour_annee AND pp.etape_num = :new.etape_num ;
-			if(nbpp=0) then
-				npp:=0;
-			else
-				select max(pp.pt_pass_num) into npp from point_passage pp where pp.tour_annee = :new.tour_annee AND pp.etape_num = :new.etape_num;
-			end if;
-		:new.pt_pass_num:=npp+1;
-		:new.ville_nom_debuter := returnVille(:new.ville_num_debuter);
-		:new.ville_nom_finir := returnVille(:new.ville_num_finir);
-		:new.pt_pass_km_arr := distance-:new.pt_pass_km_dep;
-	end if; 
-end;
-/
-
-
+-- ETAPE --
 create or replace trigger TI_ETAPE
 before insert on ETAPE
 for each row
@@ -142,30 +127,127 @@ begin
 end;
 /
 
-create or replace trigger TI_EQUIPE
-before insert on EQUIPE
+-- -----------------------------------------------------------------------------
+--       GESTION DU CLASSEMENT
+-- -----------------------------------------------------------------------------
+
+
+-- CATEGORIE --
+create or replace trigger TI_CATEGORIE
+before insert on CATEGORIE 
 for each row
-declare
-nequipe number(2);
-nbequipe number(2);
 begin
 	if inserting then
-		select count(equ.equipe_num) into nbequipe from equipe equ where equ.tour_annee = :new.tour_annee;
-			if(nbequipe=0) then
-				nequipe:=0;
-			else
-				select max(equ.equipe_num) into nequipe from equipe equ where equ.tour_annee = :new.tour_annee;
-			end if;
-		:new.equipe_num:=nequipe+1;
-		:new.equipe_pays := returnPays(:new.pays_num);
+	select seq_categorie.nextval into :new.cat_num from dual;
 	end if; 
 end;
 /
 
+-- POINT_PASSAGE --
+create or replace trigger TI_POINT_PASSAGE
+before insert on POINT_PASSAGE
+for each row
+declare
+distance number(3);
+km_arr number(3);
+npp number(3);
+nbpp number(3);
+begin
+	if inserting then
+		select count(pp.pt_pass_num) into nbpp from point_passage pp where pp.tour_annee = :new.tour_annee AND pp.etape_num = :new.etape_num ;
+			if(nbpp=0) then
+				npp:=0;
+			else
+				select max(pp.pt_pass_num) into npp from point_passage pp where pp.tour_annee = :new.tour_annee AND pp.etape_num = :new.etape_num;
+			end if;
+		:new.pt_pass_num:=npp+1;
+		:new.pt_pass_ville_nom := returnVille(:new.ville_num);
+		:new.pt_pass_km_arr := distance-:new.pt_pass_km_dep;
+	end if; 
+end;
+/
 
+-- PASSER --
+create or replace 
+trigger TI_PASSER
+before insert on PASSER
+for each row
+declare
+cl_part number(3);
+v_passer passer%ROWTYPE;
+CURSOR chk_class IS
+    SELECT * FROM passer WHERE  pass_tps > :NEW.pass_tps
+    and tour_annee = :new.tour_annee
+    and etape_num = :new.etape_num
+    and pt_pass_num = :new.pt_pass_num;
+begin
+	if inserting then
+		select count(*) into cl_part from passer WHERE  pass_tps < :NEW.pass_tps
+		and tour_annee = :new.tour_annee
+		and etape_num = :new.etape_num
+		and pt_pass_num = :new.pt_pass_num;
+		
+		cl_part:=cl_part+1;
+		:new.pass_class:=cl_part;
+		
+		OPEN chk_class;
+		LOOP
+			cl_part:=cl_part+1;
+			FETCH chk_class INTO v_passer;
+			UPDATE PASSER SET pass_class = cl_part where v_passer.part_num = passer.part_num
+			and v_passer.tour_annee = passer.tour_annee
+			and v_passer.etape_num = passer.etape_num
+			and v_passer.pt_pass_num = passer.pt_pass_num;
+			EXIT WHEN chk_class%NOTFOUND;
+		END LOOP;
+	end if; 
+end;
+/
 
+-- -----------------------------------------------------------------------------
+--       GESTION COMBATIF
+-- -----------------------------------------------------------------------------
 
+-- SPECIALISTE --
+create or replace trigger TI_SPECIALISTE
+before insert on SPECIALISTE 
+for each row
+begin
+	if inserting then
+	select seq_specialiste.nextval into :new.spe_num from dual;
+	end if; 
+end;
+/
 
+-- -----------------------------------------------------------------------------
+--       GESTION DOPAGE
+-- -----------------------------------------------------------------------------
+
+-- CONTROLE --
+create or replace trigger TI_CONTROLE
+before insert on CONTROLE
+for each row
+begin
+	if inserting then
+	select seq_controle.nextval into :new.contr_num from dual;
+	end if; 
+end;
+/
+
+-- -----------------------------------------------------------------------------
+--       GESTION DES UTILISATEURS
+-- -----------------------------------------------------------------------------
+
+-- UTILISATEUR --
+create or replace trigger TI_UTILISATEUR
+before insert on UTILISATEUR
+for each row
+begin
+	if inserting then
+	select seq_utilisateur.nextval into :new.util_num from dual;
+	end if; 
+end;
+/
 
 
 
--- Conception/Scripts/999_fonction.sql
+++ Conception/Scripts/999_fonction.sql
@@ -8,14 +8,58 @@ BEGIN
 END;
 /
 
-create or replace 
-FUNCTION returnPays(numPays pays.pays_num%type)
+create or replace FUNCTION returnPays(numPays pays.pays_num%type)
 RETURN pays.pays_nom%type IS
 nomPays pays.pays_nom%type;
 BEGIN
 	select pays_nom into nomPays from pays where pays.pays_num =  numPays;
    
    RETURN nomPays;
+END;
+/
+
+create or replace FUNCTION returnNomCycliste(num cycliste.cycliste_num%type)
+RETURN cycliste.cycliste_nom%type IS
+nom cycliste.cycliste_nom%type;
+BEGIN
+	select cycliste_nom into nom from cycliste where cycliste.cycliste_num = num;
+   RETURN nom;
+END;
+/
+
+create or replace FUNCTION returnPrenomCycliste(num cycliste.cycliste_num%type)
+RETURN cycliste.cycliste_prenom%type IS
+prenom cycliste.cycliste_prenom%type;
+BEGIN
+	select cycliste_prenom into prenom from cycliste where cycliste.cycliste_num = num;
+   RETURN prenom;
+END;
+/
+
+create or replace FUNCTION returnDateCycliste(num cycliste.cycliste_num%type)
+RETURN cycliste.cycliste_daten%type IS
+daten cycliste.cycliste_daten%type;
+BEGIN
+	select cycliste_daten into daten from cycliste where cycliste.cycliste_num = num;
+   RETURN daten;
+END;
+/
+
+create or replace FUNCTION returnNomEquipe(num equipe.equipe_num%type)
+RETURN equipe.equipe_nom%type IS
+nom equipe.equipe_nom%type;
+BEGIN
+	select equipe_nom into nom from equipe where equipe.equipe_num = num;
+   RETURN nom;
+END;
+/
+
+create or replace FUNCTION convertTemps(heure number,minu number, sec number, cent number )
+RETURN vtps in number(10)
+vtps number(10);
+BEGIN
+	vtps:=(heure*60*60*60)+(minu*60*60)+(sec*60)+cent;
+   RETURN tps;
 END;
 /
 
